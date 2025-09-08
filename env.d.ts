c45bd57d81ac6eba62ac4152164ae33a6413f98f
vincent <lutaotang93@163.com>
2025-09-08 12:18:59 +0800

    updated favicon

--- env.d.ts
+++ env.d.ts
@@ -2,11 +2,12 @@
 
 interface ImportMetaEnv {
   // Auto generate by env-parse
+  readonly VITE_USER_NODE_ENV: string
   readonly VITE_MINI_ZHIPIN_TITLE: string
   readonly VITE_MINI_ZHIPIN_API_URL: string
   readonly VITE_MINI_ZHIPIN_API_PREFIX: string
   readonly VITE_MINI_ZHIPIN_UI_PREFIX: string
-  readonly VITE_USER_NODE_ENV: string
+  readonly VITE_MINI_ZHIPIN_NOTE_NAME: string
 }
 
 interface ImportMeta {
