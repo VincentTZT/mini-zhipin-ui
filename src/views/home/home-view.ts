c8a89db99bae0007bc3ab8356df8cb6a62cd88ef
vincent <lutaotang93@163.com>
2025-09-10 07:00:03 +0800

    updated trigger chat logic

--- src/views/home/home-view.ts
+++ src/views/home/home-view.ts
@@ -286,7 +286,7 @@ export default {
               errorCallBack(res)
             }
           })
-          .finally(() => loadTriggerChatUsed())
+          .finally(() => viewObj.triggerChatUsed++)
       },
     })
 
